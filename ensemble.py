5a7ad300c1e10581a4e6c2e7d52943a4de9bf69b
Hendrik Weideman <weideh@rpi.edu>
2015-03-05 16:38:25 -0500

    Changed ensemble of nets to use geometric mean and added the newest best net, net13.

--- ensemble.py
+++ ensemble.py
@@ -81,17 +81,33 @@ def split_valid_train(data_file, labels_file, target='valid'):
     return X_requested, y_requested
 
 
-def ensemble_models(net_file_list, X, y=None, N=10):
-    num_classes = 121
-    num_samples = X.shape[0]
-    ensemble_probabilities = np.zeros((num_samples, num_classes), dtype=np.float32)
+def compute_geometric_mean(mat_list, bias_low=True):
+    if bias_low:
+        log_mat_sum = np.sum(np.log(np.array(mat_list)), axis=0)
+    else:
+        log_mat_sum = np.sum(np.log(1. - np.array(mat_list)), axis=0)
 
+    log_mat_sum /= len(mat_list)
+    if bias_low:
+        geom_mean = np.power(np.e, log_mat_sum)
+    else:
+        geom_mean = 1. - np.power(np.e, log_mat_sum)
+
+    return geom_mean
+
+
+def ensemble_models(net_file_list, X, y=None, N=10):
+    num_samples = X.shape[0]
+
+    # store the predictions from each net
+    ensemble_probabilities = []
     for net_file in net_file_list:
         print('loading net %s from disk...' % (net_file))
         with open(net_file, 'rb') as ifile:
             net = pickle.load(ifile)
-        probabilities = np.zeros((num_samples, num_classes), dtype=np.float32)
 
+        # store the predictions from each transformation for this net
+        net_probabilities = []
         for i in range(N):
             time_start = time.time()
 
@@ -103,19 +119,22 @@ def ensemble_models(net_file_list, X, y=None, N=10):
 
             # now do predictions
             proba = net.predict_proba(Xc)
-            probabilities += proba
+            net_probabilities.append(proba)
             time_end = time.time()
             print('  validation iteration %d of %d (%.1f s)' % (i + 1, N, time_end - time_start))
-        predicted_probs = probabilities / N
+
+        # compute the geometric mean of all this net's predicted probabilities
+        predicted_probs = compute_geometric_mean(net_probabilities, bias_low=True)
 
         if y is not None:
             net_loss = log_loss(y, predicted_probs)
             print('loss for net %s is %.6f' % (net_file, net_loss))
-        ensemble_probabilities += predicted_probs
+        ensemble_probabilities.append(predicted_probs)
 
-    ensemble_probabilities /= len(net_file_list)
+    # compute the geometric mean of the probabilities from all nets
+    ensemble_probabilities_mean = compute_geometric_mean(ensemble_probabilities, bias_low=True)
 
-    return ensemble_probabilities
+    return ensemble_probabilities_mean
 
 
 def generate_submission(probabilities, names, submitfile, headerfile):
@@ -137,14 +156,16 @@ if __name__ == '__main__':
     train_labels_file = join(root, 'train_labels_noaspect.npy')
     test_data_file = join(root, 'test_data_noaspect.npy')
     test_names_file = join(root, 'test_names_noaspect.npy')
-    submission_file = join(root, 'submissions', 'submit_ensemble_5_6.csv')
+    submission_file = join(root, 'submissions', 'submit_net_10_13.csv')
 
     net_file_names = [
-        'net5.pickle',
+        #'net5.pickle',
         #'net6.pickle',
         #'net7.pickle',
         #'net8.pickle',
         'net10.pickle',
+        #'net11.pickle',
+        'net13.pickle',
     ]
     net_file_list = [join(root, 'nets', nname) for nname in net_file_names]
     for fname in net_file_list:
@@ -159,7 +180,7 @@ if __name__ == '__main__':
         print('train_data_file = %s' % (train_data_file))
         print('train_labels_file = %s' % (train_labels_file))
         data, labels = split_valid_train(train_data_file, train_labels_file, target=target)
-        predicted_probs = ensemble_models(net_file_list, data, labels, N=40)
+        predicted_probs = ensemble_models(net_file_list, data, labels, N=10)
         valid_ensemble_loss = log_loss(labels, predicted_probs)
         print('loss for ensemble of %d nets is %.6f' % (len(net_file_list), valid_ensemble_loss))
     elif sys.argv[1].lower() == 'test':
@@ -170,5 +191,5 @@ if __name__ == '__main__':
         data, _ = load2d(test_data_file)
         names = np.load(test_names_file)
 
-        predicted_probs = ensemble_models(net_file_list, data, y=None, N=40)
+        predicted_probs = ensemble_models(net_file_list, data, y=None, N=50)
         generate_submission(predicted_probs, names, submission_file, header_file)
